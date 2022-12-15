0d146b3a785c1e80bec9c61ef644db95b00c310e
crosscay <caicedocarlos0306@gmail.com>
2022-12-15 16:29:54 -0500

    Routing en la App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
+import { Router } from 'react-router';
+import { Routes, Route } from react-router;
+import Ordenes from './components/paginas/Ordenes.js'
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-light'>Hola Mundo</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
+      <Routes>
+        <Route path="/" element={<Ordenes/>} />
+      </Routes>
     </div>
+    // <div className="App">
+
+      /* <h1 className='text-3xl font-light'>Hola Mundo</h1> */
+      
+    // </div>
   );
 }
 
---
+++ src/components/paginas/Ordenes.js
@@ -0,0 +1,11 @@
+import { React, Fragment } from 'react';
+
+const Ordenes = () => {
+    return ( 
+        <Fragment>
+            <h1 className='text-3xl font-light mb4'>Hola Mundo</h1>
+        </Fragment>
+     );
+}
+ 
+export default Ordenes;
