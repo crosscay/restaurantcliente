dba09d5e0b4083cf4b9e962015edf2e21edae040
crosscay <caicedocarlos0306@gmail.com>
2022-12-19 19:20:49 -0500

    Mostrando los platillos en la interfaz

--- src/components/paginas/Menu.js
+++ src/components/paginas/Menu.js
@@ -1,13 +1,52 @@
-import { React } from 'react';
 import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { FirebaseContext } from '../../firebase/index';
+import Platillo from '../ui/Platillo';
 
 const Menu = () => {
+
+    // definir el state para los platillos
+    const [ platillos, guadarPlatillos ] = useState([]);
+
+    // Context con las operaciones de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    // consultar la base de datos al cargar
+    useEffect(() => {
+        const obtenerPlatillos = () => {
+            const resultado = firebase.db.collection('productos').onSnapshot(manejarSnapshot);
+            
+        }
+        obtenerPlatillos();
+    }, []);
+
+    // Snapshot nos permite utilizar la base de datos en tiempo real de firestore
+    function manejarSnapshot(snapshot) {
+        const platillos = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        // almacenar los resultados en el state
+        guadarPlatillos(platillos);
+        console.log(platillos)
+    }
+
     return ( 
         <>
             <h1 className='text-3xl font-light mb4'>Menu</h1>
             <Link to="/nuevo-platillo"  className='bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold'>
                 Agregar Platillo
             </Link>
+
+            {platillos.map( platillo => (
+                <Platillo
+                    key={platillo.id}
+                    platillo={platillo}
+                />
+            ))}
         </>
      );
 }
--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -1,20 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useFormik } from 'formik';
-import * as Yup from 'yup'; 
+import * as Yup from 'yup';
 
 import { FirebaseContext } from '../../firebase/index';
 import { useNavigate } from 'react-router-dom';
+import FileUploader from 'react-firebase-file-uploader';
 
 const NuevoPlatillo = () => {
+
+    // state para las imagenes
+    const [ subiendo, guardarSubiendo ] = useState(false);
+    const [ progreso, guardarProgreso ] = useState(0);
+    const [ urlimagen, guardarUrlimagen] = useState('');
+
     // Context con las operaciones de firebase
     const { firebase } = useContext(FirebaseContext);
 
+    // console.log(firebase)
     // Hook para redireccionar
     const navigate = useNavigate();
 
     // validación y leer los datos del formulario
     const formik = useFormik(
-        {
+    {
             initialValues: {
                 nombre: '',
                 precio: '',
@@ -40,25 +48,64 @@ const NuevoPlatillo = () => {
             }),
             onSubmit: async (platillo) => {
                 try {
-                    console.log(platillo);
                     platillo.existencia = true;
-                    // le mando la colección donde debe crearse y el cuerpo como objeto
-                    const res = await firebase.insertarProducto("productos", { ...platillo });
-
-                    // en caso recibo un id quiere decir que se insertó
-                    if (res.id) {
-                        // Redireccionar
-                        navigate('/menu');
-                        console.log("inserción de cuerpo correcta:", res.id);
-                    }
+                    platillo.imagen = urlimagen;
+    
+                    firebase.db.collection('productos').add(platillo);
+    
+                    // Redireccionar
+                    navigate('/menu');
                 } catch (error) {
                     console.log(error);
                 }
-            }
-        }
-    )
+                // try {
+                //     console.log(platillo);
+                //     platillo.existencia = true;
+                //     // le mando la colección donde debe crearse y el cuerpo como objeto
+                //     const res = await firebase.insertarProducto("productos", { ...platillo });
 
-    return ( 
+                //     // en caso recibo un id quiere decir que se insertó
+                //     if (res.id) {
+                //         // Redireccionar
+                //         navigate('/menu');
+                //         console.log("inserción de cuerpo correcta:", res.id);
+                //     }
+                // } catch (error) {
+                //     console.log(error);
+                // }
+            }
+    });
+
+    // Todo sobre las imagenes
+    const handleUploadStart = () => {
+        guardarProgreso(0);
+        guardarSubiendo(true);
+    }
+    const handleUploadError = error => {
+        guardarSubiendo(false);
+        console.log(error);
+    }
+    const handleUploadSuccess = async nombre => {
+        guardarProgreso(100);
+        guardarSubiendo(false);
+
+        // Almacenar URL destino
+        const url = await  firebase
+                .storage
+                .ref("productos")
+                .child(nombre)
+                .getDownloadURL();
+
+        console.log(url);
+        guardarUrlimagen(url);
+    }
+    const handleProgress = progreso => {
+        guardarProgreso(progreso);
+
+        console.log(progreso);
+    }
+
+    return (
         <>
             <h1 className="text-3xl font-light mb4">Nuevo Platillo</h1>
             <div className="flex justify-center mt-10">
@@ -66,10 +113,10 @@ const NuevoPlatillo = () => {
                     <form onSubmit={formik.handleSubmit}>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">Nombre</label>
-                            <input id="nombre" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                            placeholder="Nombre Platillo" 
-                            value={formik.values.nombre} 
-                            onChange={formik.handleChange} 
+                            <input id="nombre" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Nombre Platillo"
+                            value={formik.values.nombre}
+                            onChange={formik.handleChange}
                             onBlur={formik.handleBlur}
                             />
                         </div>
@@ -83,10 +130,10 @@ const NuevoPlatillo = () => {
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">Precio</label>
-                            <input id="precio" type="number" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                            <input id="precio" type="number" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="$20"
                             min="0"
-                            value={formik.values.precio} 
+                            value={formik.values.precio}
                             onChange={formik.handleChange}
                             onBlur={formik.handleBlur}
                             />
@@ -104,7 +151,7 @@ const NuevoPlatillo = () => {
                             <select className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="categoria"
                             name="categoria"
-                            value={formik.values.categoria} 
+                            value={formik.values.categoria}
                             onChange={formik.handleChange}
                             onBlur={formik.handleBlur}
                             >
@@ -125,27 +172,51 @@ const NuevoPlatillo = () => {
                             </div>
                         ): null }
 
-                        <div className="mb-4">
+                        {/* <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
-                            <input id="imagen" type="file" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                            value={formik.values.imagen} 
+                            <input id="imagen" type="file" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            value={formik.values.imagen}
                             onChange={formik.handleChange}
                             onBlur={formik.handleBlur}
                             />
+                        </div> */}
+                        {/* ref(firebase.storage,"images") */}
+                        {/* storageRef={firebase.storage.ref("productos")} */}
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
+                            <FileUploader
+                                accept="image/*"
+                                id="imagen"
+                                name="imagen"
+                                randomizeFilename
+                                storageRef={firebase.storage.ref("productos")}
+                                onUploadStart={handleUploadStart}
+                                onUploadError={handleUploadError}
+                                onUploadSuccess={handleUploadSuccess}
+                                onProgress={handleProgress}
+                            />
                         </div>
 
-                        { formik.touched.imagen && formik.errors.imagen ? (
-                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
-                                <p className="font-bold">Hubo un error: </p>
-                                <p>{formik.errors.imagen}</p>
+                        { subiendo && (
+                            <div className="h-12 relative w-full border">
+                                <div className="bg-green-500 absolute left-0 top-0 text-white px-2 text-sm h-12 flex items-center" style={{ width: `${progreso}%` }}>
+                                    {progreso} %
+                                </div>
                             </div>
-                        ): null }
+                        ) }
+
+                        {urlimagen && (
+                            <p className="bg-green-500 text-white p-3 text-center my-5">
+                                La imagen se subió correctamente
+                            </p>
+                        ) }
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="descripcion">Descripción</label>
-                            <textarea id="descripcion" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40" 
+                            <textarea id="descripcion" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
                             placeholder="Descripción del Platillo"
-                            value={formik.values.descripcion} 
+                            value={formik.values.descripcion}
                             onChange={formik.handleChange}
                             onBlur={formik.handleBlur}
                             >
@@ -167,5 +238,5 @@ const NuevoPlatillo = () => {
         </>
      );
 }
- 
+
 export default NuevoPlatillo;
